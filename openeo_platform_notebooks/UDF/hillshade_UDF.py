5eaf00e8569aef59c3c3aafa9c0bd66491750a84
Radoslaw Guzinski <radosuav@op.pl>
2022-11-16 12:08:50 +0100

    Move hillshade UDF from notebook to Python file

---
+++ openeo_platform_notebooks/UDF/hillshade_UDF.py
@@ -0,0 +1,67 @@
+from openeo.udf import XarrayDataCube
+from openeo.udf.debug import inspect
+import numpy as np
+from hillshade.hillshade import hillshade
+
+
+def rasterize(azimuth, resolution=None):
+    # Convert the azimuth into its components on the XY-plane. Depending on the value of the
+    # azimuth either the x or the y component of the resulting vector is scaled to 1, so that
+    # it can be used conveniently to walk a grid.
+
+    azimuth = np.deg2rad(azimuth)
+    xdir, ydir = np.sin(azimuth), np.cos(azimuth)
+
+    if resolution is not None:
+        xdir = xdir * resolution[0]
+        ydir = ydir * resolution[1]
+
+    slope = ydir / xdir
+    if slope < 1. and slope > -1.:
+        xdir = 1.
+        ydir = slope
+    else:
+        xdir = 1. / slope
+        ydir = 1.
+    return xdir, ydir
+
+
+def _run_shader(sun_zenith, sun_azimuth, elevation_model, resolution_x, resolution_y):
+
+    azimuth = np.nanmean(sun_azimuth.astype(np.float32))
+    zenith = np.nanmean(sun_zenith.astype(np.float32))
+    if np.isnan(azimuth):
+        shadow = np.zeros(elevation_model.shape) + 255
+    else:
+        resolution = (float(resolution_x), float(resolution_y))
+        ray_xdir, ray_ydir = rasterize(azimuth, resolution)
+
+        # Assume chunking is already done by Dask
+        ystart = 0
+        yend = elevation_model.shape[0]
+
+        # Make sure inputs have the right data type
+        zenith = float(zenith)
+        ray = (float(ray_xdir), float(ray_ydir))
+        shadow = hillshade(elevation_model.astype(np.float32),
+                           resolution,
+                           zenith,
+                           ray,
+                           ystart,
+                           yend)
+        shadow = shadow.reshape(elevation_model.shape)
+        shadow[np.isnan(sun_azimuth)] = 255
+    return shadow
+
+
+def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
+    in_xarray = cube.get_array()
+    sun_zenith = in_xarray.sel({"bands": "sunZenithAngles"}).values.astype(np.float32)
+    sun_azimuth = in_xarray.sel({"bands": "sunAzimuthAngles"}).values.astype(np.float32)
+    elevation_model = in_xarray.sel({"bands": "DEM"}).values.astype(np.float32)
+    res_y = in_xarray.coords["y"][int(len(in_xarray.coords["y"])/2)+1] - in_xarray.coords["y"][int(len(in_xarray.coords["y"])/2)]
+    res_x = in_xarray.coords["x"][int(len(in_xarray.coords["x"])/2)+1] - in_xarray.coords["x"][int(len(in_xarray.coords["x"])/2)]
+
+    shadow = _run_shader(sun_zenith, sun_azimuth, elevation_model, res_x, res_y)
+    cube.get_array().values[0] = shadow
+    return cube
