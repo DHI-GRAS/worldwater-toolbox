75fa6223348ec2db169f9186e747e13977b34c16
walid11ghariani@gmail.com <wagh@dhigroup.com>
2026-01-29 10:50:51 +0100

    updated udp json

--- world_water_toolbox/generate_udp.py
+++ world_water_toolbox/generate_udp.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 import openeo
-from world_water_toolbox.wwt import generate_water_extent_udp
+from wwt import generate_water_extent_udp
 
 
 udp = generate_water_extent_udp(openeo.connect("openeo.vito.be"))
