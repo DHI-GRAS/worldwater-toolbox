f2e705b9ac73083783bd91d289f05433e7f4c68b
walid11ghariani@gmail.com <wagh@dhigroup.com>
2026-02-20 16:43:10 +0100

    added backends options

--- world_water_toolbox/generate_udp.py
+++ world_water_toolbox/generate_udp.py
@@ -4,8 +4,13 @@ from pathlib import Path
 import openeo
 from world_water_toolbox.wwt import generate_water_extent_udp
 
+# Choose a single backend 
+# backend = "openeo.dataspace.copernicus.eu"
+# backend = "openeo-dev.vito.be"
+backend = "openeo.vito.be"
+connection = openeo.connect(backend)
 
-udp = generate_water_extent_udp(openeo.connect("openeo.vito.be"))
+udp = generate_water_extent_udp(connection)
 
 with open(Path(__file__).parent / "single_month_extent_udp.json","w+") as f:
 
