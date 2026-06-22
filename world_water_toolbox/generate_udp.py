3449c2493ea52283e688c61aca51a22f41b3f3d0
walid11ghariani@gmail.com <wagh@dhigroup.com>
2026-06-22 14:07:06 +0200

    added generate_water_indicators_udp

--- world_water_toolbox/generate_udp.py
+++ world_water_toolbox/generate_udp.py
@@ -2,16 +2,20 @@ import json
 from pathlib import Path
 
 import openeo
-from world_water_toolbox.wwt import generate_water_extent_udp
+from world_water_toolbox.wwt import generate_water_extent_udp, generate_water_indicators_udp
 
 # Choose a single backend 
-# backend = "openeo.dataspace.copernicus.eu"
+#backend = "openeo.dataspace.copernicus.eu"
+backend = "openeofed.dataspace.copernicus.eu"
 # backend = "openeo-dev.vito.be"
-backend = "openeo.vito.be"
+#backend = "openeo.vito.be"
 connection = openeo.connect(backend)
 
 udp = generate_water_extent_udp(connection)
 
 with open(Path(__file__).parent / "single_month_extent_udp.json","w+") as f:
-
     json.dump(udp,f,indent=2)
+
+indicators_udp = generate_water_indicators_udp(connection)
+with open(Path(__file__).parent / "water_indicators.json", "w+") as f:
+    json.dump(indicators_udp, f, indent=2)
